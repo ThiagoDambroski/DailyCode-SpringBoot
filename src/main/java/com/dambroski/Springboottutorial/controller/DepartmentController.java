6da6d97e058bb0909ff0afe8faf178b8619967ec
ThiagoDambroski <dambroskic@hotmail.com>
2022-09-22 22:05:14 -0300

    loggers

--- src/main/java/com/dambroski/Springboottutorial/controller/DepartmentController.java
+++ src/main/java/com/dambroski/Springboottutorial/controller/DepartmentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,9 +24,11 @@ public class DepartmentController {
 	@Autowired
 	private DepartmentService service;
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+	
 	@PostMapping("/departments")
 	public Department saveDepartment(@Valid @RequestBody Department department) {
-		
+		LOGGER.info("inside saveDepartment of DepartmentController");
 		return service.saveDepartment(department);
 		
 	}
@@ -32,6 +36,7 @@ public class DepartmentController {
 	
 	@GetMapping("/departments")
 	public List <Department> fetchDepartament() {
+		LOGGER.info("inside fetchDepartament of DepartmentController");
 		return service.fetchDepartament();
 	}
 	
